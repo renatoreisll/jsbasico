0a9ee13b8a2ee75a9dececaa4c2566be28299988
Renato Reis <renatoreis@live.com>
2020-10-06 19:27:39 -0400

    Criacao de projeto.

---
+++ operadores.js
@@ -0,0 +1,27 @@
+// Operacoes aritimeticas;
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(8/4);
+
+// Igual
+// console.log(2 == 2);
+// console.log(2 == 3);
+
+// // Nao igual
+// console.log(2 != 2);
+// console.log(2 != 1);
+
+//Igual estrito ---> quando eh comparado o valor e o tipo (int, str..)
+// console.log(2 === "2");
+
+// //Nao igual estrito
+// console.log(3 !== "3");
+
+// Maior que
+console.log(3>2);
+console.log(2<3);
+console.log(3<2);
+console.log(2>3);
+console.log(2<=2);
---
+++ variaveis.js
@@ -0,0 +1,20 @@
+// Variavel eh um local na memoria do computador que vc pode alocar valores durante a execucao do software.
+
+var number = 10;
+console.log(number)
+
+//typeof , responsavel por trazer o tipo da variavel
+
+console.log(typeof number)
+
+var name = "Renato";
+console.log(name)
+console.log(typeof name)
+
+var n1 = 10
+var n2 = 20
+
+n1 = "1" + 5;
+
+console.log(n1 + n2);
+
