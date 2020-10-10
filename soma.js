cc2b94c09debb0e2585cffc41953222bdc76a148
Renato Reis <renatoreis@live.com>
2020-10-09 20:08:02 -0400

    Fim

--- soma.js
+++ soma.js
@@ -1,5 +1,14 @@
 
 
-//DOM = Document Object Model
+function somaValores() {
+    var n1 = document.getElementById("numberOne").value;
+    var n2 = document.getElementById("numberTwo").value;
 
-//
+    var result = parseInt(n1) + parseInt(n2);
+
+    var divResultado = document.getElementById("resultado")
+
+    divResultado.append("O resultado da soma eh:" + result)
+
+    console.log(result)
+}
